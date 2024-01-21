7f22582b61545e821ca3f3658071c05f64ff6869
SKomarov <Komarov.Sergey.sk@yandex.ru>
2024-01-21 11:31:14 +0300

    add lesoon task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+//foreach -- не меняет значения переменой 
+//while -- не знаем точное число итераций 
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+
+    }
+}
+int[] array = { 10, 22, 44, 66, 77 };
+ZeroEvenElements(array);
+PrintArray(array);
